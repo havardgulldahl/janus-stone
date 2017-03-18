5143935e9bb2d57cd405fd80c4daebd57c4a06a4
Håvard Gulldahl <havard@gulldahl.no>
2017-03-19 00:19:19 +0100

    use argparse to get date limit

--- get_comment_feed.py
+++ get_comment_feed.py
@@ -1,20 +1,42 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-import facebook
+from datetime import datetime
 import requests
 from clint.textui import colored, puts, indent
 from pprint import pprint
+import argparse
+import facebook
 
 JANUS_APP_ID='264804657310512'
 JANUS_APP_TOKEN='441046be6a35caaa163e5e69309a587c'
 JANUS_APP_TOKEN='EAACEdEose0cBAHNjPZArRvKoPZBcTNnoCSwkswOON6ZB8C2YZBjLsHRZB6vDdnU1x1X1HVR56eCcVeTZCw9EhEQ2IixAv1JeUSiu0QshElHAQzEZBxNQ6KXAGSXOtahDIYlpSC9BTCsDGkRR3eIuE5nsJzBFuVrYQMV0bPKKOgzGVB4O9xbhdFn65qaMzLw9zcZD'
 
+
+def datestring(string):
+    try:
+        value = datetime.strptime(string, '%Y-%m-%d')
+        return value.timestamp()
+    except ValueError:
+        msg = "%r is not a YYYY-MM-DD date" % string
+        raise argparse.ArgumentTypeError(msg)
+
+argp = argparse.ArgumentParser()
+argp.add_argument('--since', type=datestring, help='Date in YYYY-MM-DD format')
+argp.add_argument('--until', type=datestring, help='Date in YYYY-MM-DD format')
+
+args = argp.parse_args()
+
 graph = facebook.GraphAPI(access_token=JANUS_APP_TOKEN, version='2.8')
 
+params = {'fields': 'from,id,message,created_time,status_type,comments{from,id,like_count,message},likes{name}'}
 
-fields = {'fields': 'from,id,message,created_time,status_type,comments{from,id,like_count,message},likes{name}'}
-feed = graph.request('/dnb/feed', fields)
+if args.since:
+    params['since'] = args.since
+if args.until:
+    params['until'] = args.until
+
+feed = graph.request('/dnb/feed', params)
 
 def store_post(post):
     #pprint(post)
@@ -26,7 +48,7 @@ def store_post(post):
             colored.blue(' @ {}'.format(post['created_time'])) + \
             colored.yellow('(+{}): '.format(likes)) + \
             colored.black(message))
-    with indent(4, quote=' >'):
+    with indent(8, quote=' >'):
         for com in comments:
             puts(colored.magenta(com['from']['name']) + colored.yellow(' (+{}): '.format(com['like_count'])) + com['message'])
     puts('----------\n')
