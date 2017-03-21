84cc4b5c9df58d44813fa8ac35a0bb7e54f1c602
Håvard Gulldahl <havard@gulldahl.no>
2017-03-21 21:30:24 +0100

    add delay to fusion table inserts to avoid rate limit

--- get_comment_feed.py
+++ get_comment_feed.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+import time
 import html
 import os
 from datetime import datetime
@@ -90,25 +91,31 @@ def store_post(post):
         'kommentarer': comments_html(comments),
         'Delinger': shares,
     }
-    puts(colored.green(post['from']['name']) + \
+    puts(colored.magenta(post['from']['name']) + \
          colored.blue(' @ {}'.format(post['created_time'])))
     return fusion.insertrow(args.fusiontable, kwargs)
 
 # Wrap this block in a while loop so we can keep paginating requests until
 # finished.
+i = 0
 while True:
     try:
         # Perform some action on each post in the collection we receive from
         # Facebook.
         for post in feed['data']:
+            i = i+1
             http_code, status = store_post(post)
             if http_code > 201:
                 puts(colored.red(repr(status)))
+                puts('Error detected! Stop the slurp. Fix the error')
+                break
             else:
                 puts(colored.green(repr(status)))
+            time.sleep(0.7)
         # Attempt to make a request to the next page of data, if it exists.
-        cont = input('=================== continue? press y =================== ')
-        if cont.strip().lower() != 'y': break
+        cont = input('=================== {} records slurped. Continue? press y =================== '.format(i))
+        if cont.strip().lower() != 'y': 
+            break
         feed = requests.get(feed['paging']['next']).json()
     except KeyError:
         # When there are no more pages (['paging']['next']), break from the
