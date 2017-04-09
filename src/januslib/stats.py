5af6018b796a2376e41e50447fc917287d15f370
Håvard Gulldahl <havard@gulldahl.no>
2017-04-09 21:44:19 +0200

    add stats filter callback

--- src/januslib/stats.py
+++ src/januslib/stats.py
@@ -16,25 +16,34 @@ class JanusStatsSink(JanusSink):
         self.stat_type = stat_type
         self.created_dates = SortedDict()
         self.counted = -1
+        self.filter = None
+        self.date_stats = True  # boolean
 
     def __str__(self):
         'return pretty name'
         return '<#{} Stats({})>'.format(self.id, self.stat_type)
 
+    def set_filter(self, cb):
+        self.filter = cb
+        
     def push(self, post):
-        post_date = post.datetime_created.date().isoformat()
-        if not post_date in self.created_dates:
-            self.created_dates[post_date] = 1
-        else:
-            self.created_dates[post_date] += 1
+        if self.filter is not None:
+            if not self.filter(post): # run thru filter
+                return
+        if self.date_stats == True:
+            post_date = post.datetime_created.date().isoformat()
+            if not post_date in self.created_dates:
+                self.created_dates[post_date] = 1
+            else:
+                self.created_dates[post_date] += 1
         self.counted += 1
         
 
     def finished(self):
         with tempfile.NamedTemporaryFile(suffix='.csv', prefix='janus_stats_{}'.format(self.stat_type), delete=False) as f:
             f.write('# Janus Stone stats {} created {}\r\n'.format(self.stat_type, datetime.now().isoformat()).encode())
+            f.write('# Total count: {} ({}) '.format(self.counted, 'filtered' if self.filter else 'unfiltered')
             for dte, cnt in self.created_dates.items():
-                #puts(colored.blue(dte) + ': ' + colored.magenta(cnt), self.output)
                 f.write('{};{}\r\n'.format(dte, cnt).encode())
             puts('Finished processing {} posts. Here are the {} stats. Hope you are happy: {}'.format(self.counted, self.stat_type, f.name))
 
